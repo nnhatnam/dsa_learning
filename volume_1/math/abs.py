8b68616cc581f6014ad3f5305593bf09522b2a88
Nam Nguyen <nnhatnam91@gmail.com>
2020-08-13 23:34:17 -0500

    Add Udacity Nano Degree
    Add Udacity Nano Degree

--- volume_1/math/abs.py
+++ volume_1/math/abs.py
@@ -4,9 +4,16 @@ import math
 import cmath
 import numpy
 
+# for integer
+def abs(num):
+    mask = num >> 31
+    return (num + mask) ^ mask
+
+
 # https://graphics.stanford.edu/~seander/bithacks.html#IntegerAbs
 # python use 32 bit to represent int http://projectpython.net/chapter02/#integer-types
 # https://stackoverflow.com/a/52635807
 def fabs1(num):
-    return math.fabs(num)
+    mask = num >> 31
+    return (num + mask) ^ mask
 
