84fa399bb09b53de7cef48a137a6110f72ee5bce
Nam Nguyen <nnhatnam91@gmail.com>
2020-10-19 19:59:25 -0500

    problem 4

---
+++ Project 3/problem_4.py
@@ -0,0 +1,41 @@
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+
+    Args:
+       input_list(list): List to be sorted
+    """
+    pivot = 1
+    i, j , k = 0 , 0 , len(input_list) - 1
+    # print("Start ", input_list)
+    while j <= k:
+        if input_list[j] < pivot:
+            if i != j:
+                input_list[i], input_list[j] = input_list[j], input_list[i]      # swap i and j
+                # print("swap " , i , j)
+                i += 1
+            else:
+                j += 1
+        elif input_list[j] > pivot:
+            input_list[j], input_list[k] = input_list[k], input_list[j]     # sweap j and k
+            # print("swap ", j , k)
+            k -= 1
+        else:
+            j += 1
+        # print(i , j , k , input_list)
+    return input_list
+
+
+
+def _test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+_test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+_test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+_test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
