2a26c20dcb572b749ba3392455e93f86fbd4bc0e
Nam Nguyen <nnhatnam91@gmail.com>
2020-10-25 14:06:51 -0500

    test cases for problem 2

--- Project 3/explanation.md
+++ Project 3/explanation.md
@@ -8,3 +8,13 @@ The time complexity is `O(log(n))`
 ### Space complexity
 The space complexity is `O(1)` 
 
+## Problem 2 (Search in a Rotated Sorted Array)
+When a sorted array is rotated, there is always one half of the array still sorted and the other half is either a rotated sorted array
+or a sorted array (when the rotation happens on half of the array such in case [3, 4, 1 , 2]) . If the number we are looking for in the sorted 
+half, just perform a binary search. Otherwise, repeat the process on the other half 
+
+### Time complexity
+The time complexity is `O(log(n))`
+
+### Space complexity
+The space complexity is `O(1)` 
--- Project 3/problem_2.py
+++ Project 3/problem_2.py
@@ -1,8 +1,6 @@
-def binarySearch(input_list, lo , hi, number):
-
-
+def binary_search(input_list, lo, hi, number):
     while lo <= hi:
-        mid = (lo + hi) // 2
+        mid = (lo + hi) >> 1
         if input_list[mid] == number:
             return mid
         elif number < input_list[mid]:
@@ -28,24 +26,29 @@ def rotated_array_search(input_list, number):
     lo = 0
     hi = len(input_list) - 1
 
-
-
     while lo <= hi:
 
         mid = (lo + hi) // 2
         if input_list[mid] == number:
             return mid
 
-        if input_list[lo] <= input_list[mid]:                   # sorted array lies the left side
+        # sorted array lies on the left side
+        if input_list[lo] <= input_list[mid]:
+
+            # if the number is on the left size, perform normal binary search.
+            # Otherwise, divide right side into two halves
             if input_list[lo] <= number < input_list[mid]:
-                return binarySearch(input_list, lo , mid, number)   # perform normal binary search on the left side
+                return binary_search(input_list, lo, mid, number)
             else:
                 lo = mid + 1
-        else:                                                   # sorted array lies the right side
+        else:  # sorted array lies on the right side
+
+            # if the number is on the right size, perform normal binary search.
+            # Otherwise, divide left side into two halves
             if input_list[mid] < number <= input_list[hi]:
-                return binarySearch(input_list, mid + 1, hi)
+                return binary_search(input_list, mid + 1, hi)
             else:
-                hi = mid -1
+                hi = mid - 1
     return -1
 
 
@@ -54,6 +57,7 @@ def linear_search(input_list, number):
         if element == number:
             return index
     return -1
+
 
 def _test_function(test_case):
     input_list = test_case[0]
@@ -65,9 +69,19 @@ def _test_function(test_case):
 
 
 if __name__ == "__main__":
-    print(rotated_array_search([6, 7, 8, 9, 10, 1, 2, 3, 4], 1))
+    # Udacity test cases
     _test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
     _test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
     _test_function([[6, 7, 8, 1, 2, 3, 4], 8])
     _test_function([[6, 7, 8, 1, 2, 3, 4], 1])
     _test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+
+    # Other test cases
+    _test_function([[], 6])
+
+    _test_function([[1, 0], 1])
+    _test_function([[1, 0], 0])
+    _test_function([[1, 0], 2])
+
+    _test_function([[1], 1])
+    _test_function([[1], 0])
